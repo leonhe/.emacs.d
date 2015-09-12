9743b309589cad7ea5351ac613372f7e1fe8abff
Yuanfei He <hyf@juhuiwan.cn>
2015-09-12 11:01:12 +0800

    增加包安装地址,增加cctags配置

--- init.el
+++ init.el
@@ -9,5 +9,15 @@
 (require 'init-markdown);;初始化markdown mode
 (require 'init-org-mode);;org-mode
 (require 'init-company-mode);;初始化代码自动提示插件
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(add-hook 'c-mode-common-hook
+	  (lambda ()
+	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+	      (ggtags-mode 1))))
+
+
+(add-hook 'dired-mode-hook 'ggtags-mode)
 (provide 'init)
