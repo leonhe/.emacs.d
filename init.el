7aeb07b27a09bd24aeb9f765012edccddf15ae82
LeonHe <leonhe86@gmail.com>
2015-11-30 19:58:56 +0800

    update config

--- init.el
+++ init.el
@@ -21,6 +21,17 @@
 ;;	      (ggtags-mode 1))))
 
 
-(;;add-hook 'dired-mode-hook 'ggtags-mode)
+;;(add-hook 'dired-mode-hook 'ggtags-mode)
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/org-documents/Task/Task.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -10,25 +10,25 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 (put 'upcase-region 'disabled nil)
 ;;FIX:测试并发布BLOG文章
-(setq org-publish-project-alist
-      '(("orgfiles"
-	 :base-directory "~/Documents/blog/public/"
-	 :publishing-directory "~/Documents/public_html"
-	 :publishing-function org-html-publish-to-html
-	 :section-numbers nil
-	 :with-toc nil
-	 :html-head ""
-	 )))
+;;(setq org-publish-project-alist
+;;     '(("orgfiles"
+;;	 :base-directory "~/Documents/blog/public/"
+;;	 :publishing-directory "~/Documents/public_html"
+;;	 :publishing-function org-html-publish-to-html
+;;	 :section-numbers nil
+;;	 :with-toc nil
+;;	 :html-head ""
+;;	 )))
 ;;显示任务完成时间
 (setq org-log-done 'time)
 ;;(setq org-log-done 'note)
 ;;在周列表中显示任务
 (setq org-agenda-include-diary t)
 ;;设置任务文件列表
-(setq org-agenda-files (quote ("~/Task/Inbox"
-			       "~/Task/NextAction"
-			       "~/Task/Project"
-			       )))
+;;(setq org-agenda-files (quote ("~/Task/Inbox"
+;;			       "~/Task/NextAction"
+;;			       "~/Task/Project"
+;;			       )))
 (setq org-todo-keywords
       '((sequence "TODO(t)" "WAIT(w)" "DONE(d)")
 	))
