98fa389a03cf59847b12a4ba75d5015be7024e17
Leon He <leonhe86@gmail.com>
2016-07-10 01:15:26 +0800

    update helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -8,4 +8,6 @@
 (helm-mode 1)
 ;;keyboard bind
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-h x") 'helm-apropos)
 (provide 'init-helm)
