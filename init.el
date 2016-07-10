f1473a55628237afc30bfe91aafbb79fd1ba5aa8
Leon He <leonhe86@gmail.com>
2016-07-10 13:00:10 +0800

    add time display

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; init.el
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
 ;;(add-hook 'after-init-hook #'(lambda) () (load "")))
 (require 'init-package);;插件包处理
