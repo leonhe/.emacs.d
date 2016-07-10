93b1fccda2a918717d5c4666e2996bf5ef68b2e5
Leon He <leonhe86@gmail.com>
2016-07-10 13:26:03 +0800

    open global magit mode enable

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,5 +1,6 @@
 ;;git-emacs
 (require-package 'magit)
+(global-magit-file-mode 1)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (provide 'init-magit)
