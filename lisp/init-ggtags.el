f26336fb08dbcd78520544287b5c6f1909379ab4
Leon He <leonhe86@gmail.com>
2016-07-11 23:54:32 +0800

    add kill windows keybind

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -5,5 +5,9 @@
 (add-hook 'c-mode-common-hook
 	  (lambda ()
 	    (when (derived-mode-p 'c-mode 'c++-mode  'lua-mode)
-	      (ggtags-mode 1))))
+	      (ggtags-mode 1)
+	      )))
+
+;;keybinad
+(global-set-key (kbd "C-c M-x") 'ggtags-kill-window)
 (provide 'init-ggtags)
