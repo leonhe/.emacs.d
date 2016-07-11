a29fdaab49121d7d4ec43ab136f49122fa4146c3
Leon He <leonhe86@gmail.com>
2016-07-11 22:38:20 +0800

    add smerge keybinad

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -3,4 +3,10 @@
 (global-magit-file-mode 1)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+;;出现冲突解决热键
+(global-set-key (kbd "C-c ^ m") 'smerge-keep-mine)
+(global-set-key (kbd "C-c ^ a") 'smerge-keep-all)
+(global-set-key (kbd "C-c ^ o") 'smerge-keep-other)
+(global-set-key (kbd "C-c ^ c") 'smerge-keep-current)
+(global-set-key (kbd "C-c ^ b") 'smerge-keep-base)
 (provide 'init-magit)
