669237835c6dfdbc65fa56194d7cc819371331a9
Leon He <leonhe86@gmail.com>
2016-07-11 20:17:49 +0800

    add date time format

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -1,6 +1,16 @@
 ;;基础设置
 ;;(tool-bar-mode nil);;关闭顶部菜单栏
 (display-time-mode 1);;开启时间显示
+;;时间使用24小时制
+(setq display-time-24hr-format t)
+;;时间显示包括日期和具体时间
+(setq display-time-day-and-date t)
+;;时间栏旁边启用邮件设置
+(setq display-time-use-mail-icon t)
+;;时间的变化频率
+(setq display-time-interval 10)
+;;显示时间的格式
+(setq display-time-format "%m月%d日%A%H:%M")
 (set-keyboard-coding-system 'utf-8);;系统编码为UTF-8
 (global-linum-mode) ;;开启全局文件行数显示
 (global-font-lock-mode 1);;开启语法高亮
