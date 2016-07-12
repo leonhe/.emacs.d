d4675bb56ffb51f198c57c1e0b6622fb3d0c765d
Leon He <leonhe86@gmail.com>
2016-07-12 22:34:29 +0800

    add org-mode fetch rss content

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -7,70 +7,35 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-;;(put 'upcase-region 'disabled nil)
-;;FIX:测试并发布BLOG文章
+(setq org-feed-alist
+      '(
+	;; ("纽约时报"
+	;;  "http://cn.nytimes.com/rss.html"
+        ;;  "~/Documents/org/feed/qqnews.org" "纽约时报")
+	("大败毒"
+	 "http://news.baidu.com/n?cmd=7&loc=2354&name=%E4%B8%8A%E6%B5%B7&tn=rss"
+         "~/Documents/org/feed/qqnews.org" "大败毒")
+	))
+(setq org-feed-alist
+      '(
+	("网易大法好"
+	 "http://news.163.com/special/00011K6L/rss_newstop.xml"
+         "~/Documents/org/feed/qqnews.org" "网易大法好")
+	))
 
-(setq org-publish-project-alist
-     '(("org"
-;	 :components ("client_task" "client_task")
-	 :base-directory "~/Documents/org/sources/"
-	 :publishing-directory "~/Documents/org/public_html/"
-	 :publishing-function org-html-publish-to-html
-	 :section-numbers nil
-	 :html-head "<link rel=\"stylesheet\" href=\"http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css\" />
-<link rel=\"stylesheet\" href=\"http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css\"/>"
-	 :html-head-include-scripts "<script src=\"http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js\"/>
-<script src=\"http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js\"/>"
-	 :table-of-contents t
-	 :style-include-default nil
-	 :org-html-table-default-attributes nil
-	 :language "zh"
-	 :org-html-container-element nil
-;;	 ("client_task"
-;;	  :base-directory "~/Documents/org/sources/task/"
-;;	  :publishing-directory "/Volumes/Public/public_html/"
-;;	  :recursive t
-;;	  :publishing-function org-publish-attachment
-;;	  )
-;;	 :org-html-table-caption-above nil
-;;	 :org-html-table-default-attributes nil
-;;	 :html-table-attributes nil
-	 ;;:html-head-default-style nil
-	 ;;:html-table-row-tags ""
-	 
-	 )))
+(setq org-feed-alist
+      '(
+	("纽约时报"
+	 "http://cn.nytimes.com/rss.html"
+         "~/Documents/org/feed/qqnews.org" "纽约时报")
+	))
 
-;;这里是发布到公司的服务器上
-(setq org-publish-project-alist
-    '(("client_task"
-	 :base-directory "~/Documents/org/sources/"
-	 :publishing-directory "/Volumes/Public/public_html/"
-	 :publishing-function org-html-publish-to-html
-	 :section-numbers nil
-	 :html-head "<link rel=\"stylesheet\" href=\"http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css\" />
-<link rel=\"stylesheet\" href=\"http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css\"/>"
-	 :html-head-include-scripts "<script src=\"http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js\"/>
-<script src=\"http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js\"/>"
-	 :table-of-contents t
-	 :style-include-default nil
-	 :org-html-table-default-attributes nil
-	 :language "zh"
-;;	 :org-html-table-caption-above nil
-;;	 :org-html-table-default-attributes nil
-;;	 :html-table-attributes nil
-;;	 :html-head-default-style nil
-;;	 :html-table-row-tags ""
-	 )))
+
 ;;显示任务完成时间
 (setq org-log-done 'time)
 ;;(setq org-log-done 'note)
 ;;在周列表中显示任务
 (setq org-agenda-include-diary t)
-;;设置任务文件列表
-;;(setq org-agenda-files (quote ("~/Task/Inbox"
-;;			       "~/Task/NextAction"
-;;			       "~/Task/Project"
-;;			       )))
 (setq org-todo-keywords
       '((sequence "TODO(t)" "WAIT(w)" "DONE(d)")
 	))
