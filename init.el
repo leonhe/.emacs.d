4c5e1fdc469e67c6d2008874285ec23f43df5ed0
Leon He <leonhe86@gmail.com>
2016-09-16 12:15:13 +0800

    add go lang mode

--- init.el
+++ init.el
@@ -26,6 +26,8 @@
     (require 'init-markdown)
     (require 'init-flycheck)
 (require 'init-ggtags)
+(require 'go-mode-load)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,7 +36,7 @@
  '(org-agenda-files (quote ("~/Documents/org/todo/task.org")))
  '(package-selected-packages
    (quote
-    (flycheck-color-mode-line flycheck-tip xml-rpc undo-tree sudo-edit powerline org-page moe-theme markdown-mode magit helm-gtags goto-chg ggtags flymake-lua flycheck company-lua blog-admin ace-window))))
+    (go-mode flycheck-color-mode-line flycheck-tip xml-rpc undo-tree sudo-edit powerline org-page moe-theme markdown-mode magit helm-gtags goto-chg ggtags flymake-lua flycheck company-lua blog-admin ace-window))))
     
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ lisp/go-mode-load.el
@@ -0,0 +1,4 @@
+(require-package 'go-mode)
+(require 'go-mode-autoloads)
+
+(provide 'go-mode-load)
