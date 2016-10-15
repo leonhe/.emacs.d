9d3356b36cb2ec37b2d2baf903e1bd9e98b48e0e
Leon He <leonhe86@gmail.com>
2016-10-15 18:55:06 +0800

    update:format state display datetime

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -2,19 +2,18 @@
 ;;; Commentary:
 ;; 基础设置
 ;;; Code:
-;;(tool-bar-mode nil);;关闭顶部菜单栏
+(tool-bar-mode nil);;关闭顶部菜单栏
 (display-time-mode 1);;开启时间显示
 ;;时间使用24小时制
-(defvar display-time-24hr-format t)
+(setq display-time-24hr-format t)
 ;;时间显示包括日期和具体时间
 (setq display-time-day-and-date t)
-
 ;;时间栏旁边启用邮件设置
-(defvar display-time-use-mail-icon t)
+(setq display-time-use-mail-icon t)
 ;;时间的变化频率
-(defvar display-time-interval 10)
+(setq display-time-interval 1)
 ;;显示时间的格式
-(defvar display-time-format "%m月%d日%A%H:%M")
+(setq display-time-format "%Y/%m/%d %H:%M:%S %A")
 (set-keyboard-coding-system 'utf-8);;系统编码为UTF-8
 (global-linum-mode) ;;开启全局文件行数显示
 (global-font-lock-mode 1);;开启语法高亮
