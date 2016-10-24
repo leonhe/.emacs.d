f42cb555495641b27bc97dc04e3aa62a5c18a5c9
Leon He <leonhe86@gmail.com>
2016-10-24 19:58:03 +0800

    bind php mode show&hide mode

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -5,6 +5,6 @@
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-
+(add-hook 'php-mode-hook 'hs-minor-mode)
 (provide 'init-php)
 ;;; init-php ends here
