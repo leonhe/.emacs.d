3146fe4a8ef15a13d2de9066611edf9d5aa4454a
Leon He <leonhe86@gmail.com>
2016-10-24 08:36:44 +0800

    update init

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -61,5 +61,16 @@
 (require 'multi-term)
 (setq multi-term-program "/bin/zsh")
 (require 'tramp)
+;; (add-hook 'term-mode-hook
+;; 	  (lambda ()
+;; 	    (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+;; 	    (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next)))
+	    ;;(define-key term-raw-map (kbd "C-y" 'term-paste)))
+;;tramp
+(require 'tramp)
+(setq tramp-default-user "root")
+(setq tramp-default-method "ssh")
+(add-to-list 'tramp-default-proxies-alist
+                   '("vmbox.com" "\\`root\\'" "/ssh:%h:"))
 (provide 'init-base)
 ;;; init-base.el ends here
