7c89041b3f6395f4849edea09bfc0c61ad887b71
Leon He <leonhe86@gmail.com>
2016-11-06 12:53:34 +0800

    add:php-mode quick search document

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -36,5 +36,55 @@
 (add-hook 'php-mode-hook
           (lambda ()
             (add-hook 'c-special-indent-hook 'unindent-closure)))
+
+
+;;quick search document action
+(add-hook 'php-mode-hook 'my-php-mode-stuff)
+
+(defun my-php-mode-stuff ()
+  (local-set-key (kbd "C-c p f") 'my-php-function-lookup)
+  (local-set-key (kbd "C-c p s") 'my-php-symbol-lookup))
+
+;;打开浏览器搜索光标对应文字的文档说明
+(defun my-php-symbol-lookup ()
+  (interactive)
+  (let ((symbol (symbol-at-point)))
+    (if (not symbol)
+        (message "No symbol at point.")
+
+      (browse-url (concat "http://php.net/manual-lookup.php?pattern="
+                          (symbol-name symbol))))))
+
+;;查找php方法显示在buffer
+(defun my-php-function-lookup ()
+  (interactive)
+  (let* ((function (symbol-name (or (symbol-at-point)
+                                    (error "No function at point."))))
+         (buf (url-retrieve-synchronously (concat "http://php.net/manual-lookup.php?pattern=" function))))
+    (with-current-buffer buf
+      (goto-char (point-min))
+        (let (desc)
+          (when (re-search-forward "<div class=\"methodsynopsis dc-description\">\\(\\(.\\|\n\\)*?\\)</div>" nil t)
+            (setq desc
+              (replace-regexp-in-string
+                " +" " "
+                (replace-regexp-in-string
+                  "\n" ""
+                  (replace-regexp-in-string "<.*?>" "" (match-string-no-properties 1)))))
+
+            (when (re-search-forward "<p class=\"para rdfs-comment\">\\(\\(.\\|\n\\)*?\\)</p>" nil t)
+              (setq desc
+                    (concat desc "\n\n"
+                            (replace-regexp-in-string
+                             " +" " "
+                             (replace-regexp-in-string
+                              "\n" ""
+                              (replace-regexp-in-string "<.*?>" "" (match-string-no-properties 1))))))))
+
+          (if desc
+              (message desc)
+            (message "Could not extract function info. Press C-F1 to go the description."))))
+    (kill-buffer buf)))
+
 (provide 'init-php)
 ;;; init-php ends here
