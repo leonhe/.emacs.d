1caf53fbe5755be881a940c705761c41d67b7b25
Leon He <leonhe86@gmail.com>
2017-04-05 09:56:48 +0800

    update org mode

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - YuanfeideMacBook-Pro.local at <2017-04-04 Tue 10:16>
-(setq stored-clock-history '(("/Users/yuanfei/Note/todo/task.org" . 69) ("/Users/yuanfei/Note/todo/task.org" . 314)  ("/Users/yuanfei/Note/todo/task.org" . 423)))
+;; org-persist.el - HetekiiMac.local at <2017-03-31 Fri 20:26>
+(setq stored-clock-history '(("/Users/yuanfei/Note/todo/project.org" . 727) ("/Users/yuanfei/Note/todo/task.org" . 68)))
