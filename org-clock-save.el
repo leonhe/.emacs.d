2cd0ebb72f129b3849b40f77a02208c1604f7216
LeonHe <leonhe86@gmail.com>
2017-04-24 21:24:46 +0800

    update clock

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - HetekiiMac.local at <2017-03-31 Fri 20:26>
-(setq stored-clock-history '(("/Users/yuanfei/Note/todo/project.org" . 727) ("/Users/yuanfei/Note/todo/task.org" . 68)))
