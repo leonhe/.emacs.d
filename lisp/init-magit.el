97ccf72b834c86e7a5132be9174001b1a8635205
Leon He <leonhe86@gmail.com>
2017-05-04 10:11:19 +0800

    git stash keyboard binds

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -12,6 +12,10 @@
 (global-set-key (kbd "C-c ^ o") 'smerge-keep-other)
 (global-set-key (kbd "C-c ^ c") 'smerge-keep-current)
 (global-set-key (kbd "C-c ^ b") 'smerge-keep-base)
+
+;;stash
+(global-set-key (kbd "C-c g s ") 'magit-stash)
+(global-set-key (kbd "C-c g a") 'magit-stash-apply)
 ;;(require 'magit-gitflow)
 ;;(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 (provide 'init-magit)
