8703ece9bfca3ec2a3af08763d119fee807d577d
LeonHe <leonhe86@gmail.com>
2017-06-06 21:04:46 +0800

    update package list path

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -7,13 +7,11 @@
 
 ;;; Code:
 (require 'package) ;; You might already have this line
-
-(add-to-list 'package-archives
-	     '("melpa" . "http://elpa.emacs-china.org/melpa/")
-	     t)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/")
-	     t)
+(setq package-archives '(
+			 ("melpa" . "http://elpa.emacs-china.org/melpa/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			 ))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
