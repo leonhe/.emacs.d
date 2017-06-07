bf15728a372cae51528f4ae1811ac84d80e64833
Leon He <leonhe86@gmail.com>
2017-06-07 09:39:26 +0800

    add ignore file

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-(setq stored-clock-history '(("/Users/yuanfei/Note/todo/task.org" . 68) ("/Users/yuanfei/Note/todo/task.org" . 2073) ("/Users/yuanfei/Note/todo/task.org" . 433) ("/Users/yuanfei/Note/todo/task.org" . 430)))
-
