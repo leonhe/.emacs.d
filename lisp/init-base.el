7cb92e3b149c7e72316975d9148227412ad44422
Leon <leonhe86@gmail.com>
2017-09-06 09:07:21 +0800

    add c++ mode hook semantic

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -118,5 +118,7 @@
 	     (yas-global-mode 1)
 	     :config
 	     (add-to-list 'yas-snippet-dirs (locate-user-emacs-file "snippets")))
+(add-hook 'c++-mode 'semantic)
+
 (provide 'init-base)
 ;;; init-base.el ends here
