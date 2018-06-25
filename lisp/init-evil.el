b32bdbdce79536751bbcbb397ac732b7dd667cac
leonhe <leonhe86@gmail.com>
2018-06-25 22:09:23 +0800

    update evil conf

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -2,8 +2,20 @@
 ;;; Commentary:
 ;;; Code:
  (defvar my-leader-map (make-sparse-keymap)
-  "Keymap for \"leader key\" shortcuts.")
-     
+   "Keymap for \"leader key\" shortcuts.")
+(defun evil-toggle-input-method ()
+  "when toggle on input method, switch to evil-insert-state if possible.
+    when toggle off input method, switch to evil-normal-state if current state is evil-insert-state"
+  (interactive)
+  (if (not current-input-method)
+      (if (not (string= evil-state "insert"))
+	  (evil-insert-state))
+    (if (string= evil-state "insert")
+	(evil-normal-state)
+      ))
+  (toggle-input-method))
+
+
 (use-package evil
   :ensure t
   :config
@@ -11,9 +23,12 @@
     (evil-mode 1)
     (setq evil-auto-indent t)
     (define-key evil-normal-state-map (kbd "SPC") my-leader-map)
+;;    (global-set-key (kbd "C-\\") 'evil-toggle-input-method)
     (add-hook 'easy-hugo-mode-hook 'turn-off-evil-mode) ;;close eays-hugo-mode evil
-    )
-  :bind ( :map my-leader-map 
+   )
+  :bind (
+	 ("M-\\" . 'evil-toggle-input-method)
+	 :map my-leader-map 
 	     ("g" . magit-status)
 	     ("b" . helm-buffers-list)
 	     ("f" . helm-find-files)
