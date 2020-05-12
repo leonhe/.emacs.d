b98706a86d1b2c43f78743af60e247a824207e2a
leonhe <lhe868@gmail.com>
2020-05-12 10:10:45 +0800

    :fire: remove easy-hugo mode change to ox-hugo mode

--- local-lisp/init-blog.el
+++ local-lisp/init-blog.el
@@ -1,54 +1,54 @@
 ;;; init-org-mode.el --- org mode configure
 ;;; Commentary:
 ;;; Code:
-(defun leon/easy-hugo ()
-  (interactive)
-  (evil-define-key
-    (list 'normal 'insert 'visual 'motion)
-    easy-hugo-mode-map
-    "n" 'easy-hugo-newpost
-    "D" 'easy-hugo-article
-    "p" 'easy-hugo-preview
-    "P" 'easy-hugo-publish
-    "o" 'easy-hugo-open
-    "d" 'easy-hugo-delete
-    "e" 'easy-hugo-open
-    "c" 'easy-hugo-open-config
-    "f" 'easy-hugo-open
-    "N" 'easy-hugo-no-help
-    "v" 'easy-hugo-view
-    "r" 'easy-hugo-refresh
-    "g" 'easy-hugo-refresh
-    "s" 'easy-hugo-sort-time
-    "S" 'easy-hugo-sort-char
-    "G" 'easy-hugo-github-deploy
-    "A" 'easy-hugo-amazon-s3-deploy
-    "C" 'easy-hugo-google-cloud-storage-deploy
-    "q" 'evil-delete-buffer
-    (kbd "TAB") 'easy-hugo-open
-    (kbd "RET") 'easy-hugo-preview)
-)
-(use-package easy-hugo
-  :ensure t
-  :config
-  (setq easy-hugo-org-header nil)
-  (setq easy-hugo-server-flags "-D")
-  (setq easy-hugo-blog-number 2)
-  (setq easy-hugo-image-directory "images")
-  (setq easy-hugo-basedir "~/Org/blog")
-  (setq easy-hugo-url "https://hii8.com")
-  (setq easy-hugo-sshdomain "hii8.com")
-  (setq easy-hugo-root "/var/www/html/")
-  (setq easy-hugo-previewtime "300")
-  (setq easy-hugo-default-ext ".org")
-  (setq easy-hugo-helm-ag t)
-  (add-hook 'easy-hugo-mode-hook 'leon/easy-hugo)
-  (global-key-binding  (kbd "C-c C-e") 'easy-hugo)
-  )
+;; (defun leon/easy-hugo ()
+;;   (interactive)
+;;   (evil-define-key
+;;     (list 'normal 'insert 'visual 'motion)
+;;     easy-hugo-mode-map
+;;     "n" 'easy-hugo-newpost
+;;     "D" 'easy-hugo-article
+;;     "p" 'easy-hugo-preview
+;;     "P" 'easy-hugo-publish
+;;     "o" 'easy-hugo-open
+;;     "d" 'easy-hugo-delete
+;;     "e" 'easy-hugo-open
+;;     "c" 'easy-hugo-open-config
+;;     "f" 'easy-hugo-open
+;;     "N" 'easy-hugo-no-help
+;;     "v" 'easy-hugo-view
+;;     "r" 'easy-hugo-refresh
+;;     "g" 'easy-hugo-refresh
+;;     "s" 'easy-hugo-sort-time
+;;     "S" 'easy-hugo-sort-char
+;;     "G" 'easy-hugo-github-deploy
+;;     "A" 'easy-hugo-amazon-s3-deploy
+;;     "C" 'easy-hugo-google-cloud-storage-deploy
+;;     "q" 'evil-delete-buffer
+;;     (kbd "TAB") 'easy-hugo-open
+;;     (kbd "RET") 'easy-hugo-preview)
+;; )
+;; (use-package easy-hugo
+;;   :ensure t
+;;   :config
+;;   (setq easy-hugo-org-header nil)
+;;   (setq easy-hugo-server-flags "-D")
+;;   (setq easy-hugo-blog-number 2)
+;;   (setq easy-hugo-image-directory "images")
+;;   (setq easy-hugo-basedir "~/Org/blog")
+;;   (setq easy-hugo-url "https://heyuanfei.com")
+;;   (setq easy-hugo-sshdomain "hii8.com")
+;;   (setq easy-hugo-root "/var/www/html/")
+;;   (setq easy-hugo-previewtime "300")
+;;   (setq easy-hugo-default-ext ".org")
+;;   (setq easy-hugo-helm-ag t)
+;;   (add-hook 'easy-hugo-mode-hook 'leon/easy-hugo)
+;;   (global-key-binding  (kbd "C-c C-e") 'easy-hugo)
+;;   )
 
-(use-package ox-hugo
+ (use-package ox-hugo
   :ensure t
-  :after ox
+  :after (ox org)
   :config
   (require 'org-hugo-auto-export-mode)
   ;; :hook
@@ -79,7 +79,7 @@ See `org-capture-templates' for more information."
                  ;; It is assumed that below file is present in `org-directory'
                  ;; and that it has a "Blog Ideas" heading. It can even be a
                  ;; symlink pointing to the actual location of all-posts.org!
-                 (file+olp "~/Org/all-posts.org" "Blog Ideas")
+                 (file+olp "~/Org/blog/content-org/all-posts.org" "Blog Ideas")
                  (function org-hugo-new-subtree-post-capture-template))))
 
 (provide 'init-blog)
--- transient/history.el
+++ transient/history.el
@@ -9,8 +9,8 @@
  (magit-fetch nil)
  (magit-gitignore nil)
  (magit-log
-  ("-n256" "--graph" "--decorate" "--show-signature")
   ("-n256" "--graph" "--decorate")
+  ("-n256" "--graph" "--decorate" "--show-signature")
   ("-n256" "--graph" "--color" "--decorate" "--show-signature")
   ("-n256" "--graph" "--color" "--decorate")
   ("-n256" "--graph" "--color" "--decorate" "--show-signature" "++header")
