edc34bc49e0356e0fbe8ff75f30fe20d58cb19ca
leonhe <lhe868@gmail.com>
2020-06-12 17:23:18 +0800

    :art:  update theme

--- init.el
+++ init.el
@@ -113,28 +113,29 @@ There are two things you can do about this warning:
 
 ;; ;; setting theme
 (use-package doom-themes
- :ensure t
- :config
- ;; Global settings (defaults)
- (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-       doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+	doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
- ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
- ;; may have their own settings.
- (load-theme 'doom-dracula t)
+  ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+  ;; may have their own settings.
+  ;; (load-theme 'doom-dracula t)
+  (load-theme 'doom-vibrant t)
+  ;; (load-theme 'doom-acario-dark t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
 
- ;; Enable flashing mode-line on errors
- (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
 
- ;; Enable custom neotree theme (all-the-icons must be installed!)
- (doom-themes-neotree-config)
- ;; or for treemacs users
- (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
- (doom-themes-treemacs-config)
-
- ;; Corrects (and improves) org-mode's native fontification.
- (doom-themes-org-config)
- )
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config)
+  )
 ;; (use-package telephone-line
 ;;   :ensure t
 ;;   :init
