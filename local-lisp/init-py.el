efd532e08a234f499d02f9651a1c84461b40718c
leonhe <lhe868@gmail.com>
2020-06-12 10:21:54 +0800

    :bug: fixed pyenv mode config error

--- local-lisp/init-py.el
+++ local-lisp/init-py.el
@@ -1,16 +1,16 @@
 ;;python development env
 (use-package pyenv-mode
+  :after (elpy projectile)
   :ensure t
   :config
   (pyenv-mode t)
-(defun projectile-pyenv-mode-set ()
+  (defun projectile-pyenv-mode-set ()
   "Set pyenv version matching project name."
   (let ((project (projectile-project-name)))
     (if (member project (pyenv-mode-versions))
         (pyenv-mode-set project)
       (pyenv-mode-unset))))
-
-(add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set)
+  (add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set)
   )
 (use-package elpy
   :ensure t
