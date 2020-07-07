00aa2c110d5c2691bc62c1e71fcf92a75b583b2a
leonhe <lhe868@gmail.com>
2020-07-07 13:30:12 +0800

    :art: add typescript snippets and ignore last-package-update-day file

--- init.el
+++ init.el
@@ -780,7 +780,52 @@ There are two things you can do about this warning:
   :demand
   :config
   (centaur-tabs-mode t)
+(setq uniquify-separator "/")
+   (setq uniquify-buffer-name-style 'forward)
+   (defun centaur-tabs-buffer-groups ()
+     "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+ Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+ All buffer name start with * will group to \"Emacs\".
+ Other buffer group by `centaur-tabs-get-group-name' with project name."
+     (list
+      (cond
+	;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+	;; "Remote")
+	((or (string-equal "*" (substring (buffer-name) 0 1))
+	     (memq major-mode '(magit-process-mode
+				magit-status-mode
+				magit-diff-mode
+				magit-log-mode
+				magit-file-mode
+				magit-blob-mode
+				magit-blame-mode
+				)))
+	 "Emacs")
+	((derived-mode-p 'prog-mode)
+	 "Editing")
+	((derived-mode-p 'dired-mode)
+	 "Dired")
+	((memq major-mode '(helpful-mode
+			    help-mode))
+	 "Help")
+	((memq major-mode '(org-mode
+			    org-agenda-clockreport-mode
+			    org-src-mode
+			    org-agenda-mode
+			    org-beamer-mode
+			    org-indent-mode
+			    org-bullets-mode
+			    org-cdlatex-mode
+			    org-agenda-log-mode
+			    diary-mode))
+	 "OrgMode")
+	(t
+	 (centaur-tabs-get-group-name (current-buffer))))))
+   (centaur-tabs-headline-match)
   :init
+  ;; (setq centaur-tabs--buffer-show-groups t)
+  (setq centaur-tabs-cycle-scope 'tabs)
   (setq centaur-tabs-plain-icons t)
   (setq centaur-tabs-set-icons t)
   (setq centaur-tabs-height 32)
@@ -795,7 +840,13 @@ There are two things you can do about this warning:
 	("g t" . centaur-tabs-forward)
 	("g T" . centaur-tabs-backward)
 	)
-  )
+  :hook
+  (dired-mode . centaur-tabs-local-mode)
+  (term-mode . centaur-tabs-local-mode)
+  (dashboard-mode . centaur-tabs-local-mode)
+ (org-agenda-mode . centaur-tabs-local-mode)
+ (helpful-mode . centaur-tabs-local-mode)
+ )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
